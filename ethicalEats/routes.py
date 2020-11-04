c2c43bf4d56fdc9db4ef4ace10c1fb2f3c23fbfb
Kevin Tek <tekk@flip1.engr.oregonstate.edu>
2020-11-03 23:09:11 -0800

    Base page, index, nav bar, dropdown

--- ethicalEats/routes.py
+++ ethicalEats/routes.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from ethicalEats import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Ethical Eats"
-
+	return render_template("index.html")
