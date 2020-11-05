a517b88eefd5e81163e661e90ef8e22d52245d67
Thomas Jensen <thomas.e.jensen@gmail.com>
2020-11-05 11:55:55 -0600

    webapp.py and recipebook.html
    Demonstrates database interaction.

---
+++ ethicalEats/webapp.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+from flask import request
+
+from db_connector.db_connector import connect_to_database, execute_query
+
+# Creates Flask instance
+webapp = Flask(__name__)
+
+
+@webapp.route('/')
+def index():
+    return render_template('index.html')
+
+
+@webapp.route('/recipebook')
+def recipebook():
+    db_connection = connect_to_database()
+    query = "SELECT * from Recipes"
+    result = execute_query(db_connection, query).fetchall()
+    return render_template('recipebook.html', recipes=result)
+
+@webapp.route('/ingredients')
+def ingredients():
+    return render_template('ingredients.html')
+
+
+@webapp.errorhandler(404)
+def heh_error(e):
+    return render_template('404.html'), 404
+
+
+@webapp.errorhandler(500)
+def another_heh_error(e):
+    return render_template('500.html'), 500
+
+
+# To start flask locally
+# if __name__ == '__main__':
+#     webapp.run(debug=True)
